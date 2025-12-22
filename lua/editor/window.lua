cab36cfeb881c03dc3a450655904f2b30b12a7f8
Yofriadi Yahya <yahya.yofriadi@hendrickscorp.com>
2025-12-22 19:23:21 +0700

    fix(focus.nvim): add event trigger

--- lua/editor/window.lua
+++ lua/editor/window.lua
@@ -9,38 +9,40 @@ return {
   {
     "nvim-focus/focus.nvim",
     version = "*",
+    event = "VimEnter",
     opts = {},
     config = function(_, opts)
       require("focus").setup(opts)
 
-      local ignore_buftypes = { "nofile", "prompt", "popup" }
-      local ignore_filetypes = { "neo-tree" }
+      local ignore_buftypes = { "nofile", "prompt", "popup", "terminal" }
+      local ignore_filetypes = {
+        "neo-tree",
+        "neo-tree-popup",
+        "neo-tree-preview",
+        "sidekick_terminal",
+      }
 
       local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
 
-      vim.api.nvim_create_autocmd("WinEnter", {
+      local function apply_focus_disable()
+        local bt = vim.bo.buftype
+        local ft = vim.bo.filetype
+        local disable = vim.tbl_contains(ignore_buftypes, bt)
+          or vim.tbl_contains(ignore_filetypes, ft)
+
+        vim.w.focus_disable = disable
+        vim.b.focus_disable = disable
+      end
+
+      vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "FileType", "TermOpen" }, {
         group = augroup,
         callback = function(_)
-          if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-            vim.w.focus_disable = true
-          else
-            vim.w.focus_disable = false
-          end
+          apply_focus_disable()
         end,
-        desc = "Disable focus autoresize for BufType",
+        desc = "Disable focus autoresize for excluded buffers/windows",
       })
 
-      vim.api.nvim_create_autocmd("FileType", {
-        group = augroup,
-        callback = function(_)
-          if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.b.focus_disable = true
-          else
-            vim.b.focus_disable = false
-          end
-        end,
-        desc = "Disable focus autoresize for FileType",
-      })
+      apply_focus_disable()
     end,
   },
   {
